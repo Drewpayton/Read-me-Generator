c5db93962a2ea73e0991614df38719633a3812f8
Drew Payton <drewpayton18@gmail.com>
2022-05-30 23:13:49 -0500

    adding files

---
+++ index.js
@@ -0,0 +1,120 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const Choices = require('inquirer/lib/objects/choices');
+const { title } = require('process');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+             message: 'What is the project title?',
+             name: 'Title'
+        }, 
+        {
+            type: 'input',
+             message: 'Describe your project.',
+             name: 'Description'
+        },
+        {
+            type: 'input',
+             message: 'How do you install this application?',
+             name: 'Installation'
+        },
+        {
+            type: 'input',
+             message: 'What is your usage information?',
+             name: 'Usage'
+        },
+        {
+            type: 'input',
+             message: 'What are your contribution guildlines?',
+             name: 'Contributing'
+        },
+        {
+            type: 'input',
+             message: 'What are your testing instructions?',
+             name: 'Tests'
+        },
+        {
+            type: 'list',
+             message: 'Which license are you using?',
+             name: 'license',
+             choices: ['None', 'Apache License 2.0', 'MIT License', 'The Unlicense', 'Mozilla Public License 2.0', 'Boost Software License 1.0']
+        },
+        {
+            type: 'input',
+             message: 'What is your github username?',
+             name: 'github'
+        },
+        {
+            type: 'input',
+             message: 'What is your email?',
+             name: 'email'
+        },
+    ])
+    .then(({
+        Title,
+        Description,
+        Installation,
+        Usage,
+        Contributing,
+        Tests,
+        license,
+        github,
+        email
+    }) => {
+        const readme = ` # ${Title}
+
+        * [Description](# Description)
+        * [Installation](# Installation)
+        * [Usage](# Usage)
+        * [Contributing](# Contributing)
+        * [license](# license)
+        * [Tests](# Tests)
+        * [Questions](# github)
+
+
+
+
+        #  Description
+
+        ${Description}
+
+        # Installation
+
+        ${Installation}
+
+        # Usage
+
+        ${Usage}
+
+        # Contributing
+
+        ${Contributing}
+
+        # license
+
+        ${license}
+
+        # Tests
+
+        ${Tests}
+
+        # Questions
+        ${github}
+        ${"https://github.com/" + github}
+        ${email}
+        `
+        createReadMe(Title, readme)
+
+    });
+
+    function createReadMe(title, data){
+        fs.writeFile(`./${title}.md`, data, (err) =>{
+            if(err) {
+                console.log(err)
+            }else{
+                console.log("it worked")
+            }
+        })
+    }
